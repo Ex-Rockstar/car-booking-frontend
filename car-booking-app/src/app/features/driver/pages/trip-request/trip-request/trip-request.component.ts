68534476adfc1982bbe18853a0b8eab822538464
Ex-Rockstar <ruthirockstar2218@gmail.com>
2025-09-14 22:24:15 +0530

    added driver initial set up

---
+++ car-booking-app/src/app/features/driver/pages/trip-request/trip-request/trip-request.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-request',
+  standalone: true,
+  imports: [],
+  templateUrl: './trip-request.component.html',
+  styleUrl: './trip-request.component.css'
+})
+export class TripRequestComponent {
+
+}
