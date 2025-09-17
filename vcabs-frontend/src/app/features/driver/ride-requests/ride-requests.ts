e935b0edc32cf0eaf74c893a4e5b84b0062abdfe
Ex-Rockstar <ruthirockstar2218@gmail.com>
2025-09-17 09:54:37 +0530

    complete project set up

---
+++ vcabs-frontend/src/app/features/driver/ride-requests/ride-requests.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ride-requests',
+  imports: [],
+  templateUrl: './ride-requests.html',
+  styleUrl: './ride-requests.css'
+})
+export class RideRequests {
+
+}
