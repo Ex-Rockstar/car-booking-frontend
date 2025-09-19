0f831c455f946574a721581be12856b5e53fae21
Ex-Rockstar <ruthirockstar2218@gmail.com>
2025-09-19 10:43:43 +0530

    added login and reg and paths declared

--- vcabs-frontend/src/app/features/auth/auth-routing-module.ts
+++ vcabs-frontend/src/app/features/auth/auth-routing-module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { Login } from './login/login';
+import { Register } from './register/register';
+import {ForgotPassword} from "./forgot-password/forgot-password";
+const routes: Routes = [
+  {path:'',redirectTo: 'login', pathMatch: 'full'},
+  {path:'login', component: Login},
+  {path:'register', component: Register},
+  {path:'forgot-password', component: ForgotPassword}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
