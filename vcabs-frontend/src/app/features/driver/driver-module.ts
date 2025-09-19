950eb5284a3d3a1affd7ed5ab5c7e67c20400e8b
Ex-Rockstar <ruthirockstar2218@gmail.com>
2025-09-19 10:44:02 +0530

    added driver modules and services

--- vcabs-frontend/src/app/features/driver/driver-module.ts
+++ vcabs-frontend/src/app/features/driver/driver-module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { DriverRoutingModule } from './driver-routing-module';
-
+import { Dashboard } from './dashboard/dashboard';
+import { RideRequests } from './ride-requests/ride-requests';
+import { RideTracking } from './ride-tracking/ride-tracking';
+import { Earnings } from './earnings/earnings';
+import { Profile } from './profile/profile';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    DriverRoutingModule
-  ]
+
+  imports: [CommonModule, 
+    DriverRoutingModule, 
+    ReactiveFormsModule,Dashboard,
+    RideRequests,
+    RideTracking,
+    Earnings,
+    Profile,],
 })
-export class DriverModule { }
+export class DriverModule {}
